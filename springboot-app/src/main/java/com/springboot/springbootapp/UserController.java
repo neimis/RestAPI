9c6051bd631f0638ff377b5afde27ba0e15bc1e1
Cezar <Cezar@host.docker.internal>
2022-12-21 15:43:10 +0100

    UserController.class->einige Kommentare ergänzt

--- springboot-app/src/main/java/com/springboot/springbootapp/UserController.java
+++ springboot-app/src/main/java/com/springboot/springbootapp/UserController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+ *Die Klasse beinhaltet eine GET Methode, die einen String Parameter (/user/name)
+ *entgegen nimmt und abhängig von der Uhrzeit einen String als Antwort zurückgibt.
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -24,16 +27,20 @@ public class UserController {
 	@GetMapping(path = "/{name}")
 	public String getName(@PathVariable String name) {
 		
+//		Festlegung einiger Uhrzeiten
 		LocalTime jetzt = LocalTime.now();
 		LocalTime mitternacht=LocalTime.of(00, 00);
 		LocalTime morgens=LocalTime.of(06, 00);
 		LocalTime mittag=LocalTime.of(13, 00);
 		LocalTime abbends=LocalTime.of(19, 00);
 	
+//		Festlegung des SQL Befehls
 		String sql="insert into LOGIN(NAME,LOGINDATE) values ('"+name+"',CURRENT_TIMESTAMP)";
 		
+//		Ausführung des SQL Befehls
 		jdbcTemplate.execute(sql);
 		
+//		Begrüßung je nach Uhrzeit
 		if(jetzt.isAfter(mitternacht)&jetzt.isBefore(mittag)) {
 			return "Guten Morgen "+name+"!"; 
 			
